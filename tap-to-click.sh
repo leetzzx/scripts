14cb5d33fea023c61147835e985ff2af3b45e8fb
leetz <ltzzx12138@gmail.com>
2022-07-15 12:35:39 +0800

    tap to click

---
+++ tap-to-click.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+#
+# tap-to-click.sh
+# Copyright (C) 2022 leetz <leetz@leetz-vltwx0>
+#
+# Distributed under terms of the MIT license.
+#
+xinput --set-prop 12 359 1 
+fcitx &
+picom -d 
+variety &
+xscreensaver --nosplash &
